d9b5f2266351bb0bdc4c8e05d1f54d89fcf6310d
Miguel Vargas <33642048+miguelvargasg@users.noreply.github.com>
2019-10-16 23:10:55 -0400

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,7 +1,12 @@
-from collections import deque
-def is_palindrome(p):
 
-    md1 = deque(p)
+ # Palindrome 
+
+from collections import deque
+
+#  Defining a function named as palindrome
+def is_palindrome(inputpal):
+
+    md1 = deque(inputpal)
     md2 = []
     md2 = deque(maxlen=len(md1))
 
@@ -9,9 +14,9 @@ def is_palindrome(p):
         md2.appendleft(x)
 
 
-    if type(p) is not str:
+    if type(inputpal) is not str:
         raise ValueError
-    if not p:
+    if not inputpal:
         return False
     if deque(md1) == deque(md2):
         return True
